33d3d33af3d39e10df8e6a557c2f94a86008fca4
JaCh23 <jach2355@gmail.com>
2025-09-28 12:34:18 +0000

    Deploying to gh-pages from @ JaCh23/JaCh23.github.io@318eb3bf36fa63c0ed95ecbbe8c430e0e3a0aaec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,78 +23,51 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-first-runner-up-in-israeli-national-hackathon",
+        },{id: "news-first-runner-up-in-israeli-national-hackathon",
           title: 'First Runner-Up in Israeli National Hackathon',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/2022-06-30-hackru/";
+              window.location.href = "/news/2022-06-30-hackru.html";
             },},{id: "news-invitation-to-judging-panel-of-cybersecurity-and-ai-hackathon",
           title: 'Invitation to Judging Panel of Cybersecurity and AI Hackathon',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/2025-04-11-aisp/";
+              window.location.href = "/news/2025-04-11-aisp.html";
             },},{id: "news-primary-teaching-assistant-for-post-graduate-module",
           title: 'Primary Teaching Assistant for Post-graduate Module',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/2025-04-16-storytellingta/";
+              window.location.href = "/news/2025-04-16-storytellingta.html";
             },},{id: "news-mentorship-national-innovation-challenge-top-8-finalist-and-awarded-5-000",
           title: 'Mentorship - National Innovation Challenge Top 8 Finalist and Awarded $5,000',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/2025-04-30-innochallenge/";
+              window.location.href = "/news/2025-04-30-innochallenge.html";
             },},{id: "news-mentorship-national-robotics-competition-national-top-3-placing",
           title: 'Mentorship - National Robotics Competition National Top 3 Placing',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/2025-09-09-nrc/";
+              window.location.href = "/news/2025-09-09-nrc.html";
             },},{id: "projects-can-you-hear-me",
           title: 'Can You Hear Me?',
           description: "Automatic Speech Recognition Pipeline for Distress Intelligence.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/01-2025-asrDistress/";
+              window.location.href = "/projects/01-2025-asrDistress.html";
             },},{id: "projects-gogoguru",
           title: 'GogoGuru',
           description: "Enhancing Motorists Communication through Personalised Co-Pilot.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/04-2024-traffic-innochallenge/";
+              window.location.href = "/projects/04-2024-traffic-innochallenge.html";
             },},{id: "projects-project-recogneyes",
           title: 'Project RecognEyes',
           description: "Have You Seen This Man? Now, AI Has.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/04-2025-recogneyes/";
+              window.location.href = "/projects/04-2025-recogneyes.html";
             },},{id: "projects-too-small-to-handle",
           title: 'Too Small to Handle?',
           description: "A Holistic Evaluation Framework towards Entity Resolution under Constrained Context Windows.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/11-2024-LLMeval/";
+              window.location.href = "/projects/11-2024-LLMeval.html";
             },},{
         id: 'social-email',
         title: 'email',
