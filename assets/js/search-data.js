4ff3cbd1f94d84942e4b5c5d097bb1910898c9bb
JaCh23 <jach2355@gmail.com>
2025-12-11 14:53:37 +0000

    Deploying to gh-pages from @ JaCh23/JaCh23.github.io@76bf49eb4cc7951dbe51edc27374b35bc753ebf0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,17 +38,6 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2025/transformersOccumRazor/";
-          
-        },
-      },{id: "post-a-post-with-formatting-and-links",
-        
-          title: "a post with formatting and links",
-        
-        description: "march &amp; april, looking forward to summer",
-        section: "Posts",
-        handler: () => {
-          
             window.location.href = "/blog/2015/test/";
           
         },
